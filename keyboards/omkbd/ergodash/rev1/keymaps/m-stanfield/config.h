d16f2693c973bb46006bba9b2581db169fbdd72c
m-stanfield <mstan44@gmail.com>
2023-11-29 22:32:56 -0500

    second keymap setup

--- keyboards/omkbd/ergodash/rev1/keymaps/m-stanfield/config.h
+++ keyboards/omkbd/ergodash/rev1/keymaps/m-stanfield/config.h
@@ -20,8 +20,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* Select hand configuration */
+#define QMK_ESC_OUTPUT F1 // usually COL
+#define QMK_ESC_INPUT D5 // usually ROW
+#define QMK_LED B0
+#define QMK_SPEAKER C6
 
+/* Select hand configuration */
 #define MASTER_LEFT
-// #define MASTER_RIGHT
+//#define MASTER_RIGHT
 // #define EE_HANDS
